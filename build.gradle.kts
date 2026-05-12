5e0ff9ef37bbfcba9458241284abf7f5ec3c5738
john-t-dev <johntoshokan.dev@proton.me>
2026-05-12 14:31:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Base64
+
 plugins {
     kotlin("jvm") version "2.0.0"
     id("com.vanniktech.maven.publish") version "0.28.0" // The API Bridge Plugin
@@ -9,7 +11,7 @@ repositories {
 }
 
 group = "io.github.john-t-dev"
-version = "1.0.6"
+version = "1.0.7"
 
 mavenPublishing {
     publishToMavenCentral("CENTRAL_PORTAL") // Explicitly targets the new API
@@ -45,7 +47,7 @@ signing {
     val signingKeyBase64 = System.getenv("ORG_GRADLE_PROJECT_signingKey")
     val signingPassword = System.getenv("ORG_GRADLE_PROJECT_signingPassword")
     if (signingKeyBase64 != null) {
-        val signingKey = String(java.util.Base64.getDecoder().decode(signingKeyBase64))
+        val signingKey = StringBase64.getDecoder().decode(signingKeyBase64))
         useInMemoryPgpKeys(signingKey, signingPassword)
     }
 }
